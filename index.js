034a3e76b7617733dfa622caa0b99ca47be423ab
Moreno Preto <moreno.preto@live.com>
2020-07-27 03:11:49 -0300

    carrousel armado y funcionando

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let h3 = document.getElementsByTagName('h3');
 let p = document.getElementsByTagName('p');
 
 
-console.log(changeTags);
+
 
 // ------------HAGO MODO NOCTURNO------------
 mN.addEventListener('click', changeMode);
@@ -28,7 +28,91 @@ function changeMode(){
     changeTags[i].classList.toggle('font-dark');
     
     console.log([i]+changeTags[i].classList);    
-    }
-    
-
+    } 
 }
+
+// ---------- CREO OBJETOS PARA CREAR EL CARROUSEL -------
+
+
+class liCarrousels {
+    constructor(liN,url,titulo,user,heart,download,expand,show) {
+        this._liN = liN;
+        this._url = url;
+        this._titulo = titulo;
+        this._user = user;
+        this._expand = expand;
+        this._download = download;
+        this._heart = heart;
+        this._show = show;
+    }
+}
+let ulCarrousel = document.getElementById('ul-carrousel');
+let li1= new liCarrousels(document.getElementById('li1'),"url'(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhKwjXzadLTpAToxpatiZxy6pLbAsVRJb8ASbgOEAcE5Y9EI&s'","Nueva imagen","","","","",true);
+let li2= new liCarrousels(document.getElementById('li2'),"url'(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhKwjXzadLTpAToxpatiZxy6pLbAsVRJb8ASbgOEAcE5Y9EI&s'","Nueva imagen","","","","",true);
+let li3= new liCarrousels(document.getElementById('li3'),"url'(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhKwjXzadLTpAToxpatiZxy6pLbAsVRJb8ASbgOEAcE5Y9EI&s'","Nueva imagen","","","","",true);
+let li4= new liCarrousels(document.getElementById('li4'),"url'(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhKwjXzadLTpAToxpatiZxy6pLbAsVRJb8ASbgOEAcE5Y9EI&s'","Nueva imagen","","","","",false);
+let li5= new liCarrousels(document.getElementById('li5'),"url'(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhKwjXzadLTpAToxpatiZxy6pLbAsVRJb8ASbgOEAcE5Y9EI&s'","Nueva imagen","","","","",false);
+let liArray = [li1,li2,li3,li4,li5];
+let flechasIzqDer = document.getElementsByClassName('square-i');
+
+flechasIzqDer[0].addEventListener('click',moverCarrouselIzq);
+flechasIzqDer[1].addEventListener('click',moverCarrouselDer);
+
+function moverCarrouselIzq(){
+    for(i=0;i<liArray.length;i++){        
+        
+           if(liArray[i]._liN.classList[1]=="primer-li"){
+            liArray[i]._liN.classList.add('oculto2');
+            liArray[i]._liN.classList.remove('primer-li');
+                      
+           } else if(liArray[i]._liN.classList[1]=="segundo-li"){
+            liArray[i]._liN.classList.add('primer-li');
+            liArray[i]._liN.classList.remove('segundo-li');
+            // console.log(liArray[i]._liN);
+        }else if(liArray[i]._liN.classList[1]=="tercer-li"){
+            liArray[i]._liN.classList.add('segundo-li');
+            liArray[i]._liN.classList.remove('tercer-li');
+            // console.log(liArray[i]._liN);
+        }else if(liArray[i]._liN.classList[1]=="oculto1"){
+            liArray[i]._liN.classList.add('tercer-li');
+            liArray[i]._liN.classList.remove('oculto1');
+            // console.log(liArray[i]._liN);
+        }else if(liArray[i]._liN.classList[1]=="oculto2"){
+            liArray[i]._liN.classList.add('oculto1');
+            liArray[i]._liN.classList.remove('oculto2');
+            // console.log(liArray[i]._liN);
+        }else{console.log("no anda")}
+        // }
+    }
+}
+function moverCarrouselDer(){
+    for(i=0;i<liArray.length;i++){        
+        
+           if(liArray[i]._liN.classList[1]=="primer-li"){
+            liArray[i]._liN.classList.add('segundo-li');
+            liArray[i]._liN.classList.remove('primer-li');
+                      
+           } else if(liArray[i]._liN.classList[1]=="segundo-li"){
+            liArray[i]._liN.classList.add('tercer-li');
+            liArray[i]._liN.classList.remove('segundo-li');
+            // console.log(liArray[i]._liN);
+        }else if(liArray[i]._liN.classList[1]=="tercer-li"){
+            liArray[i]._liN.classList.add('oculto1');
+            liArray[i]._liN.classList.remove('tercer-li');
+            // console.log(liArray[i]._liN);
+        }else if(liArray[i]._liN.classList[1]=="oculto1"){
+            liArray[i]._liN.classList.add('oculto2');
+            liArray[i]._liN.classList.remove('oculto1');
+            // console.log(liArray[i]._liN);
+        }else if(liArray[i]._liN.classList[1]=="oculto2"){
+            liArray[i]._liN.classList.add('primer-li');
+            liArray[i]._liN.classList.remove('oculto2');
+            // console.log(liArray[i]._liN);
+        }else{console.log("no anda")}
+        // }
+    }
+}
+
+
+
+
